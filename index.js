39c4db8e222df87010aea6c87d53fa3e1552e795
Zannatul-Fahmida <jannatulfahmida15@gmail.com>
2021-11-11 12:49:22 +0600

    order api added

--- index.js
+++ index.js
@@ -18,28 +18,81 @@ async function run() {
     await client.connect();
     const database = client.db('toy_store');
     const toysCollection = database.collection('toys');
+    const ordersCollection = database.collection('orders');
 
     // GET API
     app.get('/toys', async (req, res) => {
-        const cursor = toysCollection.find({});
-        const toys = await cursor.toArray();
-        res.send(toys);
+      const cursor = toysCollection.find({});
+      const toys = await cursor.toArray();
+      res.send(toys);
     });
 
     // GET Single Toy
     app.get('/toys/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const toy = await toysCollection.findOne(query);
-        console.log(toy);
-        res.json(toy);
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const toy = await toysCollection.findOne(query);
+      console.log(toy);
+      res.json(toy);
     })
 
     // POST API
     app.post('/addToys', async (req, res) => {
-        const newToy = req.body;
-        const result = await toysCollection.insertOne(newToy);
-        res.json(result)
+      const newToy = req.body;
+      const result = await toysCollection.insertOne(newToy);
+      res.json(result)
+    });
+
+    // GET Orders API
+    app.get('/orders', async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const orders = await cursor.toArray();
+      res.json(orders);
+    });
+
+    //add orders in database
+    app.post("/addOrders", (req, res) => {
+      ordersCollection.insertOne(req.body).then((result) => {
+        res.json(result);
+      });
+    });
+
+    // get all order by email query
+    app.get("/myOrders/:email", async (req, res) => {
+      const cursor = ordersCollection.find({ email: req.params.email });
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    // GET Single orders
+    app.get('/order/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const ordered = await ordersCollection.findOne(query);
+      res.json(ordered);
+    })
+
+    //Update Status
+    app.put('/order/:id', async (req, res) => {
+      const id = req.params.id;
+      const updatedOrders = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: updatedOrders.status
+        },
+      };
+      const result = await ordersCollection.updateOne(filter, updateDoc, options);
+      res.json(result);
+    })
+
+    // DELETE orders
+    app.delete('/order/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
     });
 
   }
