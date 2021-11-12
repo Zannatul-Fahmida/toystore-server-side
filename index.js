ef47b144459a3b73888c78ddafe5d5eb45246fc2
Zannatul-Fahmida <jannatulfahmida15@gmail.com>
2021-11-12 12:58:52 +0600

    problem

--- index.js
+++ index.js
@@ -3,16 +3,9 @@ const app = express();
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
-const admin = require("firebase-admin");
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
-
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
 
 app.use(cors());
 app.use(express.json())
@@ -154,7 +147,7 @@ async function run() {
     })
 
     //update users to admin
-    app.put('/users/admin', verifyToken, async (req, res) => {
+    app.put('/users/admin', async (req, res) => {
       const user = req.body;
       const requester = req.decodedEmail;
       if(requester){
